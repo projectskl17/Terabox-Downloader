2455f068713a704f3fa292447efed6eb7f797a1a
projectskl17 <hermit0bot@gmail.com>
2024-10-11 17:40:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,21 @@
 from os import environ
 
-
 # BOT CONFIG
-API_ID = environ.get("API_ID", 123456)  # api id
-API_HASH = environ.get("API_HASH", "")  # api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "")  # bot token
+API_ID = environ.get("API_ID")  # api id
+API_HASH = environ.get("API_HASH")  # api hash
+BOT_TOKEN = environ.get("BOT_TOKEN")  # bot token
 
 # REDIS
-REDIS_HOST = environ.get("REDIS_HOST", "localhost")  # redis host uri
-REDIS_PORT = environ.get("REDIS_PORT", 12345)  # redis port
-REDIS_PASSWORD = environ.get(
-    "REDIS_PASSWORD", "q8vcRgGfoZgwKT6irvJ6AixPs1lFZdW8"
-)  # redis password
+REDIS_HOST = environ.get("REDIS_HOST")  # redis host uri
+REDIS_PORT = environ.get("REDIS_PORT")  # redis port
+REDIS_PASSWORD = environ.get("REDIS_PASSWORD")  # redis password
 
-
-ADMINS = [6791744215]
-OWNER_ID = 6791744215  # Replace with your Telegram user ID
-PRIVATE_CHAT_ID = -100  # CHAT WHERE YOU WANT TO STORE VIDEOS
-USER_CHANNEL = -100
-DUMP_CHANNEL = -100
-
+# Admin and Chat Configuration
+ADMINS = list(map(int, environ.get("ADMINS", "6791744215").split(',')))  # admin ids
+OWNER_ID = int(environ.get("OWNER_ID", 6791744215))  # Replace with your Telegram user ID
+PRIVATE_CHAT_ID = int(environ.get("PRIVATE_CHAT_ID", -100))  # Chat where you want to store videos
+USER_CHANNEL = int(environ.get("USER_CHANNEL", -100))
+DUMP_CHANNEL = int(environ.get("DUMP_CHANNEL", -100))
 
 # Config
-COOKIE = environ.get("COOKIE", "")
+COOKIE = environ.get("COOKIE")  # cookie
